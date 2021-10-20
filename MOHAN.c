7a5f79b3fc342dbf49743e4e23e262585be4fd36
mohankrishna1905 <92290900+mohankrishna1905@users.noreply.github.com>
2021-10-20 12:28:28 +0530

    Add files via upload

---
+++ MOHAN.c
@@ -0,0 +1,21 @@
+/*C program to print student details */
+#include<stdio.h>
+void main ()
+{
+	// variable Declarations
+	int age;
+	float cgpa;
+	char initial;
+	
+	// Initialization
+	age = 21;
+	cgpa = 7.0;
+	initial = 'B';
+	
+	// Output
+	printf("Student age : %i\n",age);
+	printf("CGPA of the student : %f\n",cgpa);
+	printf("Student surname starts with : %c",initial);
+	
+	
+	}
