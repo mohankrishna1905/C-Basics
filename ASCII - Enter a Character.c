50cc0e1a342aae913ec8d4e461ddd9e1f30b9e00
mohankrishna1905 <92290900+mohankrishna1905@users.noreply.github.com>
2021-11-09 12:31:20 +0530

    Add files via upload

---
+++ ASCII - Enter a Character.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{   
+    char ch;
+	printf("Enter a character (A-Z or a-z  ):");
+	scanf("%c", &ch);
+	printf("Corresponding ASCII number for %c is %d", ch, ch);
+	
+}
