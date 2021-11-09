c937573931e1d7171435df82e85bff6935f630bf
mohankrishna1905 <92290900+mohankrishna1905@users.noreply.github.com>
2021-11-09 11:57:41 +0530

    Add files via upload

---
+++ Product of the Nmbers using Scanf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+
+{
+	int a, b, c, d, p;
+	printf("Enter the Four Numbers is");
+	scanf("%d%d%d%d", &a, &b, &c, &d);
+	p=a*b*c*d;
+	printf("product of %d, %d, %d and %d is: %d", a, b, c, d, p);
+}
+
