219a8579bfa9bba5c94301b9cac22467574d82fa
mohankrishna1905 <92290900+mohankrishna1905@users.noreply.github.com>
2021-11-09 11:39:43 +0530

    Add files via upload

---
+++ Usage of scanf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+
+{
+	int a, b, c;
+	printf("Enter Three Numbers");
+	scanf("%d%d%d", &a, &b, &c);
+	printf("Number 1: %d\n", a);
+	printf("Number 2: %d\n", b);
+	printf("Number 3: %d\n", c);
+}
+
