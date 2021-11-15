dd5baad4b796258a44ef8b5ef1e6e40e330f3756
mohankrishna1905 <92290900+mohankrishna1905@users.noreply.github.com>
2021-11-15 12:09:54 +0530

    Add files via upload

---
+++ ASCII VALUES.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	printf("enter ASCII value");
+	scanf("%d %d %d",&a,&b,&c);
+	printf("%c%c%c",a+95,b+95,c+95);
+}
+
+//* INPUT : 4 2 21 *//
+//* OUTPUT : cat*//	
