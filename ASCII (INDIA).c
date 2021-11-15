c9f1578a64551b8ea13e39bf0d43a18cf13ace57
mohankrishna1905 <92290900+mohankrishna1905@users.noreply.github.com>
2021-11-15 12:20:04 +0530

    Add files via upload

---
+++ ASCII (INDIA).c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d,e;
+	printf("Enter a ASCII value");
+	scanf("%d%d%d%d%d",&a, &b, &c, &d, &e);
+	printf("%c%c%c%c%c", a+64,b+64,c+64,d+64,e+64);
+}
+
+//*INPUT : 9 14 4 9 1*//
+//*OUTPUT : INDIA *//
